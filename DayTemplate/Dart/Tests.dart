efee9713bbdd2bf5b21d80020817e4b8e6ce709a
petersjl <petersjl@rose-hulman.edu>
2023-11-30 22:01:31 -0500

    Initial setup

---
+++ DayTemplate/Dart/Tests.dart
@@ -0,0 +1,26 @@
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+import '../../Utils/DartUtils.dart';
+import 'Solution.dart';
+
+void main(){
+  late Object input;
+
+  setUp(() {
+    input = parseInput(Utils.readToString('../testinput.txt'));
+  });
+
+  test("Check parse input works correclty", () {
+    expect(input, "Solution1");
+  });
+
+  group("Check sample input passes for part", () {
+    test("1", () {
+      expect(solvePart1(input), "Solution1");
+    });
+    test("2", () {
+      expect(solvePart2(input), "Fail");
+    });
+  });
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# AdventOfCode2023
+The advent of code for 2023
+
+Calendar:
